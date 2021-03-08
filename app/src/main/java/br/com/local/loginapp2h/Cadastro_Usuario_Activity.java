d5d68afe009a7938b6d9d548f133733318fab8ec
Laercio-Note <lndsilva@hotmail.com>
2021-03-08 20:22:36 -0300

    criação de activitys

---
+++ app/src/main/java/br/com/local/loginapp2h/Cadastro_Usuario_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.loginapp2h;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro_Usuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro__usuario_layout);
+    }
+}
