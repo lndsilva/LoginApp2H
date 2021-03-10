673e531889f9f578cb5bb22329ea6b671a29a839
Laercio-Note <lndsilva@hotmail.com>
2021-03-10 20:22:15 -0300

    componentes

---
+++ app/src/main/java/br/com/local/loginapp2h/Componentes_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.loginapp2h;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Componentes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.componentes_layout);
+    }
+}
